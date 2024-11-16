166fd231a80e0bee441a3e0fb01448c40b93b898
kysch13 <124356972+kysch13@users.noreply.github.com>
2024-11-16 09:28:02 -0500

    Add missing plugin

--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
-      favicon: "./src/img/favicon.svg",
+      favicon: "",
     }),
   ],
   module: {
