ea94557a78288685ed67dc2fee12dc8a47ff6a35
kysch13 <124356972+kysch13@users.noreply.github.com>
2024-11-19 22:18:21 -0500

    Add todo object with task array and actions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import taskBuilder from "./taskBuilder.js";
 
-const tasks = [];
-tasks.push(taskBuilder('bingo', 'bango', 'bongo', '2024-11-19T00:00:00', 'high'));
-tasks.push(taskBuilder('bingo', 'bango', 'bongo', '2024-11-19T00:00:00', 'high'));
-tasks.push(taskBuilder('bingo', 'bango', 'bongo', '2024-11-19T00:00:00', 'high'));
-tasks.push(taskBuilder('bingo', 'bango', 'bongo', '2024-11-19T00:00:00', 'high'));
-tasks.push(taskBuilder('bingo', 'bango', 'bongo', '2024-11-19T00:00:00', 'high'));
+const todo = {
+    groups: [],
+    tasks: [],
+    addTask: function (title, status, desc, due, priority) {
+        this.tasks.push(taskBuilder(title, status, desc, due, priority));
+    },
+    deleteTask: function (idNum) {
+        for (let i=0; i<this.tasks.length; i++) {
+            if (this.tasks[i].idNum === idNum) {
+                this.tasks.splice(i, 1);
+            }
+        }
+    }
+}
 
-console.log(tasks);
+todo.addTask('Hello', 'pending', 'description', '2024-11-19T00:00:00', 'high');
+todo.addTask('Hello2', 'pending', 'description', '2024-11-29T00:00:00', 'high');
+todo.addTask('Hello3', 'pending', 'description', '2025-12-19T00:00:00', 'low');
+console.log(todo.tasks);
+todo.deleteTask(1);
+console.log(todo.tasks);
