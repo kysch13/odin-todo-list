48d88f881781e05d22620ab292bc24d9fe758e0b
kysch13 <124356972+kysch13@users.noreply.github.com>
2024-11-20 22:02:47 -0500

    Add attributes to task element

--- src/dom.js
+++ src/dom.js
@@ -1,7 +1,12 @@
 const elementTask = function (task) {
         let container = elementMaker('div', undefined, 'task-container');
         let title = elementMaker('h2', undefined, 'task-title');
+        let desc = elementMaker('span', undefined, 'task-desc');
+        let due = elementMaker('span', undefined, 'task-due');
+        let priority = elementMaker('span', undefined, 'task-priority');
         title.innerText = task.title;
+        desc.innerText = task.desc;
+        due.innerText = task.due
         container.appendChild(title);
         return container;
 }
