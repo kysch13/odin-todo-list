ff5c93bb36fa9b6bbe2f568a7a524bf49072bf4a
kysch13 <124356972+kysch13@users.noreply.github.com>
2024-11-21 22:25:03 -0500

    Add event listener with actions for delete button

--- src/index.js
+++ src/index.js
@@ -1,39 +1,28 @@
+import "./fonts.css";
 import "./styles.css";
 import {taskBuilder, groupBuilder} from "./builders.js";
-import { elementTask } from "./dom.js";
+import {todo} from "./todo.js";
+import { makeTaskElem } from "./elements.js";
+import {clearMain, renderMain} from "./render.js";
 
 
-const todo = {
-    _groups: [],
-    _tasks: [],
-    addGroup: function (title) {
-        this._groups.push(groupBuilder(title));
-    },
-    addTask: function (title, status, desc, due, priority) {
-        this._tasks.push(taskBuilder(title, status, desc, due, priority));
-    },
-    deleteTask: function (idNum) {
-        // find task with matching id and remove from list
-        for (let i=0; i<this._tasks.length; i++) {
-            if (this._tasks[i].idNum === idNum) {
-                this._tasks.splice(i, 1);
-            }
-        }
-    }
-}
+
 
 todo.addTask('Hello', 'pending', 'description', '2024-11-19T00:00:00', 'high');
 todo.addTask('Hello2', 'pending', 'description', '2024-11-29T00:00:00', 'high');
 todo.addTask('Hello3', 'pending', 'description', '2025-12-19T00:00:00', 'low');
+todo.addTask('Medium Task', 'pending', 'description', '2025-12-19T00:00:00', 'medium');
 todo.addTask('Wash dishes', 'pending', 'wash the pots and pans', '2024-11-20T00:00:00', 'low');
-console.log(todo._tasks);
-todo.deleteTask(1);
-console.log(todo._tasks);
+todo.deleteTask(3);
 todo.addGroup('hello');
-console.log(todo._groups);
 
-const main = document.getElementById('main');
-todo._tasks.forEach(task => {
-    let taskDiv = elementTask(task);
-    main.appendChild(taskDiv);
+renderMain();
+
+main.addEventListener('click', (e) => {
+    let elem = e.target;
+    if (elem.classList.contains('task-delete')){
+        todo.deleteTask(Number(elem.dataset.taskId));
+        clearMain();
+        renderMain();
+    }
 });
