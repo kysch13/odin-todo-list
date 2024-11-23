137702fc3d39bbc5dabf5c6fad003e83e778f5a6
kysch13 <124356972+kysch13@users.noreply.github.com>
2024-11-23 11:56:09 -0500

    Add FontAwesome module, change button text to icons

--- src/elements.js
+++ src/elements.js
@@ -1,28 +1,66 @@
+import { library, findIconDefinition, icon } from '@fortawesome/fontawesome-svg-core'
+import { faSquarePen, faSquareXmark } from '@fortawesome/free-solid-svg-icons'
+
+
 function makeTaskElem (task) {
     const container = makeElem('div', undefined, 'task-container');
     container.setAttribute('data-task-id', task.idNum);
-    const children = [];
+    const statusCont = makeElem('div', undefined, 'task-status');
+    const taskDetails = makeElem('div', undefined, 'task-details');
+    const taskUI = makeElem('div', undefined, 'task-ui');
+    const detailsChildren = [];
+    const uiChildren = [];
+    statusCont.appendChild(makeStatusCheckbox(task.idNum));
     // Create html elements for task details
-    children.push(makeElem('h2', task.title, 'task-title'));
-    children.push(makeElem('span', task.desc, 'task-desc'));
-    children.push(makeElem('span', task.due, 'task-due'));
-    children.push(makeElem('span', task.priority, 'task-priority', task.priority));
-    // Create buttons for task actions
-    children.push(makeTaskActionsElem(task.idNum));
-    children.forEach(child => {
-        container.appendChild(child);
+    detailsChildren.push(makeElem('h2', task.title, 'task-title'));
+    detailsChildren.push(makeElem('span', task.desc, 'task-desc'));
+    detailsChildren.push(makeElem('span', task.due, 'task-due'));
+    uiChildren.push(makeElem('span', task.priority, 'task-priority', task.priority));
+    uiChildren.push(makeTaskActionsElem(task.idNum));
+
+    detailsChildren.forEach(child => {
+        taskDetails.appendChild(child);
     })
+    uiChildren.forEach(child => {
+        taskUI.appendChild(child);
+    })
+    container.appendChild(statusCont);
+    container.appendChild(taskDetails);
+    container.appendChild(taskUI);
+   
     return container;
+}
+
+function makeStatusCheckbox (id) {
+    const form = makeElem('form');
+    const label = makeElem('label');
+    const checkbox = makeElem('input');
+    const check = makeElem('span');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('data-task-id', id);
+    form.appendChild(label);
+    label.appendChild(checkbox);
+    label.appendChild(check);
+    return form;
 }
 
 function makeTaskActionsElem (id) {
     const actionsCont = makeElem('div', undefined, 'task-actions');
-    const deleteBtn = makeElem('button', 'DELETE', 'task-delete');
-    const editBtn = makeElem('button', 'EDIT', 'task-edit');
+    const deleteBtn = makeElem('button', undefined, 'task-delete');
+    const editBtn = makeElem('button', undefined, 'task-edit');
+    
+    // Add FontAwesome icons
+    const editIcon = icon(faSquarePen);
+    Array.from(editIcon.node).map((n) => editBtn.appendChild(n));
+    const deleteIcon = icon(faSquareXmark);
+    Array.from(deleteIcon.node).map((n) => deleteBtn.appendChild(n));
+    
     deleteBtn.setAttribute('data-task-id', id);
     editBtn.setAttribute('data-task-id', id);
-    actionsCont.appendChild(deleteBtn);
+    
     actionsCont.appendChild(editBtn);
+    actionsCont.appendChild(deleteBtn);
+    
     return actionsCont;
 }
 
